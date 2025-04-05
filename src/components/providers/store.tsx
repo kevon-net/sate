bb276c9df80cf2c444050dbd9fa2b23222fc261d
Kevon Kibochi <devokrann@gmail.com>
2025-04-05 08:03:48 +0300

    chore (store): remove memoization function

--- src/components/providers/store.tsx
+++ src/components/providers/store.tsx
@@ -1,26 +1,29 @@
 'use client';
 
-import React, { useMemo, useRef } from 'react';
-
+import React, { useRef } from 'react';
 import { Provider } from 'react-redux';
-
 import { makeStore, AppStore } from '@/libraries/redux/store';
 import { updateSession } from '@/libraries/redux/slices/session';
+import { generateUUID } from '@/utilities/generators/id';
 
-export default function Store({ children }: { children: React.ReactNode }) {
-  const storeRef = useRef<AppStore>(undefined);
+export default function Store({
+  session,
+  children,
+}: {
+  session?: { id: string };
+  children: React.ReactNode;
+}) {
+  const storeRef = useRef<AppStore | null>(null);
 
-  const store = useMemo(() => {
-    if (storeRef.current) return storeRef.current;
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
 
-    // Create the store instance the first time this renders
-    const newStore = makeStore();
+  // Always update store with the latest props
+  const store = storeRef.current;
 
-    // initialize store
-    newStore.dispatch(updateSession({ id: 'sid-1' }));
-
-    return newStore;
-  }, []);
+  // initialize store
+  if (session) store.dispatch(updateSession(session || { id: generateUUID() }));
 
   return <Provider store={store}>{children}</Provider>;
 }
