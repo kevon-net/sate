81799a1f63ba2a3ddda815a745ad55bafda89df6
Kevon Kibochi <dev@kevon.net>
2025-05-23 14:46:40 +0300

    fix: add missing async params logic chore: upadte blog detail page render method

--- src/app/(marketing)/blog/[postTitle-postId]/page.tsx
+++ src/app/(marketing)/blog/[postTitle-postId]/page.tsx
@@ -3,9 +3,29 @@ import LayoutPage from '@/components/layout/page';
 import { typeParams } from '../layout';
 import { extractUuidFromParam } from '@/utilities/helpers/string';
 import { redirect } from 'next/navigation';
+import { linkify } from '@/utilities/formatters/string';
 
-export default async function Post({ params }: { params: typeParams }) {
-  const postId = extractUuidFromParam(params['postTitle-postId']);
+export const dynamic = 'force-static';
+export const revalidate = 3600;
+
+export async function generateStaticParams() {
+  // const { data: posts, error } = await postsGet();
+  const { data: posts }: { data: any } = { data: null };
+
+  // if (error) throw error;
+  if (posts == null) return [];
+
+  return posts.map((p: any) => ({
+    'postTitle-postId': `${linkify(p.title)}-${p.id}`,
+  }));
+}
+
+export default async function Post({
+  params,
+}: {
+  params: Promise<typeParams>;
+}) {
+  const postId = extractUuidFromParam((await params)['postTitle-postId']);
 
   if (!postId) redirect('/not-found');
 
