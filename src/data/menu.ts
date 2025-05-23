230a4c802599e92a15fd660dbcf6df665e707891
Kevon Kibochi <dev@kevon.net>
2025-05-23 11:09:23 +0300

    feat: setup menu section

---
+++ src/data/menu.ts
@@ -0,0 +1,58 @@
+export const menuCtaList = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Summer Berry',
+    price: 8,
+    ingredients: 'Raspberries, Blackberries',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1490474418585-ba9bad8fd0ea?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Coconut Tart',
+    price: 10,
+    ingredients: 'Blueberries, Graham cracker crumbs',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1503146234394-631200675614?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Pumpkin Cookies',
+    price: 11,
+    ingredients: 'Pumpkin, Sugar, Butter',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1556040221-a1efce785fcc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Cookies Cream Cheese',
+    price: 14,
+    ingredients: 'Sugar, Butter, Eggs',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1468777675496-5782faaea55b?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Chocolate Cupcakes',
+    price: 14,
+    ingredients: 'Chocolate, Eggs, Vanilla',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1649531794884-b8bb1de72e68?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Chocolate Cupcakes',
+    price: 14,
+    ingredients: 'Chocolate, Eggs, Vanilla',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1675092789086-4bd2b93ffc69?q=80&w=1989&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Soft shell crab',
+    price: 14,
+    ingredients: 'Chicken, Potato, Salad',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1626081438186-3d0f9532b6b0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Soft shell crab',
+    price: 14,
+    ingredients: 'Chicken, Potato, Salad',
+  },
+];
