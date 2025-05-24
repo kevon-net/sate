e9ef0eaca4a84b280a35f0371fe202760964969b
Kevon Kibochi <dev@kevon.net>
2025-05-24 13:22:26 +0300

    chore: update dimmed text color to dark.3

--- src/styles/resolver.ts
+++ src/styles/resolver.ts
@@ -7,12 +7,14 @@ const appResolver: CSSVariablesResolver = (theme) => ({
 
   light: {
     '--mantine-color-body': `${theme.white}`,
+    '--mantine-color-dimmed': `var(--mantine-color-dark-3)`,
     '--mantine-color-text': `var(--mantine-color-dark-6)`,
     '--mantine-color-default-border': `var(--mantine-color-gray-2)`,
   },
 
   dark: {
     '--mantine-color-body': `${theme.black}`,
+    '--mantine-color-dimmed': `var(--mantine-color-dark-3)`,
     '--mantine-color-text': `var(--mantine-color-dark-0)`,
     '--mantine-color-default-border': `var(--mantine-color-gray-2)`,
   },
